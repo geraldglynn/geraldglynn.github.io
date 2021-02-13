ca57a27a1074105ca37a8608d3006fe0c89432d3
geraldglynn <geraldglynn@gmail.com>
2021-02-13 20:12:40 +0000

    Update

--- atm/app.js
+++ atm/app.js
@@ -1,38 +1,15 @@
-function keypadContent(){
-  return `
-<table id="keypad" class="keypad">
-  <tr>
-    <td onclick="keypad(this)">7</td>
-    <td onclick="keypad(this)">8</td>
-    <td onclick="keypad(this)">9</td>
-  </tr>
-  <tr>
-    <td onclick="keypad(this)">4</td>
-    <td onclick="keypad(this)">5</td>
-    <td onclick="keypad(this)">6</td>
-  </tr>
-  <tr>
-    <td onclick="keypad(this)">1</td>
-    <td onclick="keypad(this)">2</td>
-    <td onclick="keypad(this)">3</td>
-  </tr>
-  <tr>
-    <td onclick="keypad(this)">#</td>
-    <td onclick="keypad(this)">0</td>
-    <td onclick="keypad(this)">*</td>
-  </tr>
-</table>
-`
-}
-
 function appLoad(){
-  document.getElementById("atm-controls").innerHTML = keypadContent();
+  // document.getElementById("atm-controls").innerHTML = keypadContent();
 }
 
 function keypad(elem){
-  var enteredValue = elem.innerHTML  
-  var currentValue = document.getElementById("atm-screen").innerHTML;
+  var currentValue = document.getElementById("value-input").innerHTML;
+  if(currentValue.length > 3) return
+  var enteredValue = elem.innerHTML
+  if(isNaN(Number(enteredValue))) return
+  
+  debugger
   var newValue = currentValue + enteredValue;
   if(newValue == 0) return
-  document.getElementById("atm-screen").innerHTML = newValue;
+  document.getElementById("value-input").innerHTML = newValue;
 }
